403b91d3efa73ec9e8f171139ddf3ddd171354f3
pnranjani <ranjani@iprimitus.com>
2016-02-04 04:36:57 +0000

    ticket.rb changed

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,8 +3,56 @@ class Ticket < ActiveRecord::Base
   belongs_to :customer
   
   before_create :generate_tickets_number
-  before_create :generate_booking_number
- 
+ # before_create :generate_booking_number
+ before_create :check_cancelled_date
+
+  
+  def generate_tickets_number
+    a = ('A'..'Z').to_a.shuffle.first(3).join
+    self.number_1 = a
+    b = [*'0'..'9', *'A'..'Z'].to_a.shuffle.first(3).join   
+    self.number_2 = b
+  end
+  
+  def check_cancelled_date
+    reference_number = self.number_1 + self.number_2.to_s
+    if Ticket.any?
+      ticket_numbers = Ticket.all.pluck(:booking_number)
+      if ticket_numbers.include? reference_number
+        ticket = Ticket.where(:booking_number => reference_number).first
+        ticket_status = ticket.ticket_status
+         if ticket_status == "Cancelled"
+           if ticket.cancelled_date < Date.today - 3.months
+             self.booking_number = reference_number
+           else
+             need to generate new ticket number, check all conditions
+           end
+         else
+           need to generate new ticket number, check all conditions
+         end
+      else
+        self.booking_number = reference_number
+      end
+    else
+     self.booking_number = reference_number
+    end
+    self.ticket_status = "Confirmed"
+  end
+    
+  
+=begin
+
+    
+  
+  def check_journey_date
+     if ticket.journey_date < Date.today - 2.years
+       self.booking_number = reference_number
+     else
+       self.booking_number = "ABCneww number"
+     end
+  end
+
+
   def generate_tickets_number
      x = ('A'..'Z').to_a.shuffle.first(3).join
       if x == ("EKA" || "SEL" ||  "BAR" ||"RAC" || "MON" || "ETI" || "AMA")
@@ -39,6 +87,6 @@ class Ticket < ActiveRecord::Base
     end
     self.ticket_status = "Confirmed"
   end
-  
- 
+=end
+   
 end
