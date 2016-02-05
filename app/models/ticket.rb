d474c2453bdf013d1900aea7952c7b8ebba752b9
pnranjani <ranjani@iprimitus.com>
2016-02-05 02:43:56 +0000

    Ticket.rb corrected

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,11 +2,54 @@ class Ticket < ActiveRecord::Base
   
   belongs_to :customer
   
-  before_create :generate_tickets_number
- # before_create :generate_booking_number
- before_create :check_cancelled_date
+  before_create :generate_booking_number
+  before_create :check_valid_number
+  #before_create :check_cancelled_date
 
   
+  def generate_booking_number
+    a = ('A'..'Z').to_a.shuffle.first(3).join
+    number_1 = a
+    b = [*'0'..'9', *'A'..'Z'].to_a.shuffle.first(3).join   
+    number_2 = b    
+    self.booking_number = number_1 + number_2.to_s    
+    check_valid_number
+  end
+  
+  def check_valid_number
+    number = booking_number
+    not_to_be_used = ["SELFIE", "BARNEY", "RACHEL", "MONICA", "ETIHAD", "AMAZON"]
+    if not_to_be_used.include? number
+      generate_booking_number 
+    elsif number.split("").first(3).join == "EKA"
+      generate_booking_number
+    else
+      if Ticket.any?
+        booked_numbers = Ticket.all.pluck(:booking_number)
+        if booked_numbers.include? number
+          booked_ticket = Ticket.where(:booking_number => number).first
+          if (booked_ticket.ticket_status == "Cancelled") && (booked_ticket.cancelled_date < Date.today-3.months)
+             self.booking_number = number
+          elsif (booked_ticket.ticket_status == "Cancelled") && (booked_ticket.cancelled_date > Date.today-3.months)
+            generate_booking_number
+          end
+          if booked_ticket.journey_date < Date.today - 2.years
+            self.booking_date = number
+          else
+            generate_booking_number
+          end
+        else
+          self.booking_number = number
+        end
+      else
+        self.booking_number = number
+      end
+    end
+    self.ticket_status = "Confirmed"
+  end
+ 
+    
+=begin
   def generate_tickets_number
     a = ('A'..'Z').to_a.shuffle.first(3).join
     self.number_1 = a
